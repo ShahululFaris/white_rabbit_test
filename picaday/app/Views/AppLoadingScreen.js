5685a08d1a0d0b1dbf7b2a799c885140df52843e
shahulul1 <Shahulul@gmail.com>
2026-04-17 23:46:06 +0530

    Background color green

--- picaday/app/Views/AppLoadingScreen.js
+++ picaday/app/Views/AppLoadingScreen.js
@@ -40,6 +40,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: "white"
+        backgroundColor: "green"
     }
 });
